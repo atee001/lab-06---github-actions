385293203afbfde7e61295c72e8f397efb92b8aa
atee001 <atee001@ucr.edu>
2021-05-09 13:26:37 -0700

    Rectangle.cpp

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,23 +1,25 @@
-#pragma once 
-
-#include "rectangle.hpp"
+#include "../header/rectangle.hpp"
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-void set_Width(int w) {
+Rectangle::Rectangle() : width(0), height(0){}
+
+Rectangle::Rectangle(int w, int h) : width(w), height(h) {}
+
+void Rectangle::set_width(int w) {
 	width = w;
 }
 
-void set_Height(int h) {
+void Rectangle::set_height(int h) {
 	height = h;
 }
 
-int area() {
+int Rectangle::area() {
 	return width * height;
 }
 
-int perimeter() {
+int Rectangle::perimeter() {
 	return (2 * width) + (2 * height);
 }
