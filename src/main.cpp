0a01c3779acc2351c32d35d51636565ab969e478
atee001 <atee001@ucr.edu>
2021-05-09 13:26:26 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "../header/rectangle.hpp"
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	Rectangle* r = new Rectangle();
+	cout << "Rectangle area : " << r->area() << endl;
+
+
+return 0;
+}
